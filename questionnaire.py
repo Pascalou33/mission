94ab14dc4a0697b68c8f4259a2f9e4c4b34b39f9
Pascal COURBOT <pascal.courbot@gmail.com>
2023-01-07 14:32:14 +0100

    Ajout du numéro de question

--- questionnaire.py
+++ questionnaire.py
@@ -16,8 +16,8 @@ class Question:
         q = Question(data["titre"], choix, bonne_reponse[0])
         return q
 
-    def poser(self):
-        print("QUESTION")
+    def poser(self, nbre_question):
+        print("QUESTION N°" + str(nbre_question+1))
         print("  " + self.titre)
         for i in range(len(self.choix)):
             print("  ", i+1, "-", self.choix[i])
@@ -68,8 +68,9 @@ class Questionnaire:
         print("Nombre de questions: " + str(len(self.questions)))
         print()
         
-        for question in self.questions:
-            if question.poser():
+        for i in range(len(self.questions)):
+            question = self.questions[i]
+            if question.poser(i):
                 score += 1
         print("Score final :", score, "sur", len(self.questions))
         return score
