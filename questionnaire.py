a8e5a95eb00ffcc7b79ba478b40ea337208b3275
Pascal COURBOT <pascal.courbot@gmail.com>
2023-01-07 12:42:15 +0100

    Intégration fichier JSON et poser une question

--- questionnaire.py
+++ questionnaire.py
@@ -1,23 +1,3 @@
-# PROJET QUESTIONNAIRE V3 : POO
-#
-# - Pratiquer sur la POO
-# - Travailler sur du code existant
-# - Mener un raisonnement
-#
-# -> Définir les entitées (données, actions)
-#
-# Question
-#    - titre       - str
-#    - choix       - (str)
-#    - bonne_reponse   - str
-#
-#    - poser()  -> bool
-#
-# Questionnaire
-#    - questions      - (Question)
-#
-#    - lancer()
-#
 
 import json
 
@@ -28,9 +8,12 @@ class Question:
         self.choix = choix
         self.bonne_reponse = bonne_reponse
 
-    def FromData(data):
-        # ....
-        q = Question(data[2], data[0], data[1])
+    def FromJsonData(data):
+        choix = [i[0] for i in data["choix"]]
+        bonne_reponse = [i[0] for i in data["choix"] if i[1]]
+        if len(bonne_reponse) != 1 :
+            return None
+        q = Question(data["titre"], choix, bonne_reponse[0])
         return q
 
     def poser(self):
@@ -107,5 +90,9 @@ json_data = file.read()
 file.close()
 
 questionnaire_data = json.loads(json_data)
+
+questionnaire_data_questions = questionnaire_data["questions"]
+q = Question.FromJsonData(questionnaire_data_questions[0])
+q.poser()
 
 print()
