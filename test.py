1f2ce94c9d816b3edbc8aa13546cc3e1c6216002
Pascal COURBOT <pascal.courbot@gmail.com>
2023-01-28 14:53:01 +0100

    ajout patch pour input

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import unittest
+from unittest.mock import patch
 
 def additionner(a,b):
     return a+b
+
+
+def conversion_nombre():
+    num_str = input("Rentrer un nombre : ")
+    return int(num_str)
 
 
 class TestUnitaireDemo(unittest.TestCase):
@@ -11,13 +17,27 @@ class TestUnitaireDemo(unittest.TestCase):
     def tearDown(self):
         print("tearDown")
         
-    def test_additionner1(self):
-        print("test_additionner1")
+    def test_additionner_nombre_positif(self):
+        print("test_additionner_nombre_positif")
         self.assertEqual(additionner(5,10), 15)
+        self.assertEqual(additionner(6,20), 26)
+        self.assertEqual(additionner(6,-6), 0)
 
-    def test_additionner2(self):
-        print("test_additionner2")
-        self.assertEqual(additionner(6,10), 16)
+    def test_additionner_nombre_negatif(self):
+        print("test_additionner_nombre_negatif")
+        self.assertEqual(additionner(6,-10), -4)
+        self.assertEqual(additionner(-6,-10), -16)
+        self.assertEqual(additionner(-6,10), 4)
+        
+    def test_conversion_nombre_valide(self):
+        print("test_conversion_nombre_valide")
+        with patch("builtins.input", return_value="10"):
+            self.assertEqual(conversion_nombre(), 10)
+        
+    def test_conversion_entree_invalide(self):
+        print("test_conversion_entree_invalide")
+        with patch("builtins.input", return_value="dzrg"):
+            self.assertRaises(ValueError, conversion_nombre)
 
 
 unittest.main()
